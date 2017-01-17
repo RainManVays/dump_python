2de13b3dc7c6543390c6c0962c84da047a9005d5
RainManVays <ldf0@yandex.ru>
2017-01-17 17:42:52 +0300

    Update recursive_find_bettwen_Char.py

--- recursive_find_bettwen_Char.py
+++ recursive_find_bettwen_Char.py
@@ -9,3 +9,24 @@ def find_beetween_Char(row, s):
             row[row.find(s + find_item + s) + len(s + find_item + s):], s)
 
 find_beetween_Char(strr, '#')
+
+
+
+
+
+
+strr = "asdasgnksfg sd #ITEM1# ,ADFASF (#ITEM2#)  "
+
+arrs = []
+
+
+def find_beetween_Char(row, s, arr):
+    if len(row) >= 3:
+        find_item = row.partition(s)[-1].partition(s)[0]
+        arr.append(find_item)
+        find_beetween_Char(
+            row[row.find(s + find_item + s) + len(s + find_item + s):], s, arr)
+
+find_beetween_Char(strr, '#', arrs)
+for it in arrs:
+    print(it)
